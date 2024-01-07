b6ba772baa2282ce52d85c9e4e5746c542418c86
CP-Rahul <rahulcp1401@gmail.com>
2024-01-07 23:26:12 +0530

    Added chat functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected with id', socket.id);
+
+  socket.on('join room', (data) => {
+    console.log('joined in room ', data)
+    socket.join(data);
+  });
+  
+  let = room = '123'
+  socket.on('new msg', (data) => {
+    io.to(room).emit('send', data);
+    console.log(data);
+  });
+
+});
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const user = 'dummy'
-  res.render('index', {user})
-});
-
+  res.render('index');
+})
 server.listen(3000, () => {
   console.log('server is live');
 });
